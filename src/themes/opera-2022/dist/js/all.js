7b5d91149b8b3bc73dfa6ac71493254efd6ab838
Zubaer <zubaer56@gmail.com>
2022-01-14 13:55:26 +0600

    header anchor

--- src/themes/opera-2022/dist/js/all.js
+++ src/themes/opera-2022/dist/js/all.js
@@ -1,6 +1,6 @@
-!function (document, Drupal, $) {
-  'use strict'; // Remove this line
-
-  console.log('Global JS');
-}(document, Drupal, jQuery);
+// !((document, Drupal, $) => {
+//   'use strict';
+//   // Remove this line
+//   console.log('Global JS');
+// })(document, Drupal, jQuery);
 //# sourceMappingURL=global.js.map

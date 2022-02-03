2e98d6cf08a47e5003ccb4affc9f3155860a9412
Zubaer <zubaer56@gmail.com>
2022-02-03 11:55:07 +0600

    Sticky header changes

--- src/themes/opera-2022/src/patterns/global/global.js
+++ src/themes/opera-2022/src/patterns/global/global.js
@@ -1,33 +1,33 @@
-// !((document, Drupal, $) => {
-//   'use strict';
+!((document, Drupal, $) => {
+  'use strict';
 
-//   // Remove this line
-//   console.log('Global JS');
-// })(document, Drupal, jQuery);
-window.onscroll = function changeClass() {
-  var navBar = document.getElementsByClassName('l-header');
-  var layoutHeight = document.querySelector('.layout').offsetHeight;
-  let windowHeight = window.innerHeight;
-  var scrollPosY = window.pageYOffset | document.body.scrollTop;
+  window.onscroll = function changeClass() {
+    var navBar = document.getElementsByClassName('l-header');
+    var layoutHeight = document.querySelector('.layout').offsetHeight;
+    let windowHeight = window.innerHeight;
+    var scrollPosY = window.pageYOffset | document.body.scrollTop;
 
-  if (document.querySelector('#admin-bar') !== null) {
-    var adminBarHeight = document.querySelector('#admin-bar').offsetHeight;
-  }
-  else {
-    var adminBarHeight = 0;
-  }
+    if (document.querySelector('#admin-bar') !== null) {
+      var adminBarHeight = document.querySelector('#admin-bar').offsetHeight;
+    }
+    else {
+      var adminBarHeight = 0;
+    }
 
-  if (adminBarHeight > 1) {
-    adminBarHeight = document.querySelector('#admin-bar').offsetHeight;
-  }
-  else {
-    adminBarHeight = 100;
-  }
+    if (adminBarHeight > 1) {
+      adminBarHeight = document.querySelector('#admin-bar').offsetHeight;
+    }
+    else {
+      adminBarHeight = 100;
+    }
 
-  if (scrollPosY > adminBarHeight) {
-    navBar[0].classList.add('sticky_header');
-  }
-  else if (scrollPosY <= adminBarHeight) {
-    navBar[0].classList.remove('sticky_header');
-  }
-};
+    if (scrollPosY > adminBarHeight) {
+      navBar[0].classList.add('sticky_header');
+    }
+    else if (scrollPosY <= adminBarHeight) {
+      navBar[0].classList.remove('sticky_header');
+    }
+  };
+
+  console.log('Global JS');
+})(document, Drupal, jQuery);
